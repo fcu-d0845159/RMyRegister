4a8c9fa1f503983da483fd502bc306c699b595ee
D0845159 <Winlifetime3d12>
2022-05-14 07:19:21 +0800

    Initial commit

---
+++ app/src/main/java/fcu/mp/rmyregister/MainActivity.java
@@ -0,0 +1,24 @@
+package fcu.mp.rmyregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static boolean VALID_USER = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!VALID_USER) {
+            Intent intent = new Intent();
+            intent.setClass(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+}
